4849cccb81781f71b40ae0a3dd1cf2875f1e30db
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2023-06-21 11:20:23 -0300

    execute epoch, user reward

--- contracts/StakeManagerSimplified.sol
+++ contracts/StakeManagerSimplified.sol
@@ -8,7 +8,7 @@ contract StakeManager is ERC20 {
 
     ERC20 stakedToken;
 
-    uint256 public constant MP_APY = 1; 
+   uint256 public constant MP_APY = 1; 
     uint256 public constant STAKE_APY = 1; 
     uint256 public constant MAX_BOOST = 1; 
     uint256 public constant MAX_MP = 1; 
@@ -19,27 +19,10 @@ contract StakeManager is ERC20 {
         uint256 balance;
         uint256 multiplier;
         uint256 multiplierUpdate;
-        uint256 epoch;
     }
 
-    struct Epoch {
-        uint256 startTime;
-        uint256 totalReward;    
-    }
-
-    uint256 currentEpoch;
-    uint256 pendingReward;
-    
-    uint256 public constant EPOCH_SIZE = 1 week;
-
-    mapping (uint256 => Epoch) epoch;
     mapping (address => Account) account;
 
-
-    constructor() {
-        epoch[0].startTime = now();
-
-    }
 
     function increaseBalance(uint256 _amount, uint256 _time) external {
         accounts[msg.sender].balance += _amount;
@@ -75,25 +58,6 @@ contract StakeManager is ERC20 {
         accounts[_vault].multiplier += mp;
     }
 
-    function executeEpochReward() external {
-        if(now() > epoch[currentEpoch].startTime + EPOCH_SIZE){
-            uint256 epochReward = stakedToken.balanceOf(this) - pendingReward;
-            epoch[currentEpoch].totalReward = epochReward;
-            pendingReward += epochReward;
-            currentEpoch++;
-            
-            epoch[currentEpoch].startTime = now();
-        }
-
-    }
-
-    function executeUserReward(uint256 _epoch, address _vault) external {
-        uint256 userReward = getRewardsEmissions(_vault, epoch[_epoch].totalReward);
-        pendingReward -= userReward;
-        stakedToken.transfer(_vault, userReward);
-
-    }
-
     function calcInitialMultiplierPoints(uint256 _amount, uint256 _time) pure public returns(uint256) {
         return _amount * (_time + 1);
     }
@@ -105,10 +69,9 @@ contract StakeManager is ERC20 {
     }
 
 
-    function getRewardsEmissions(address _vault, uint256 _totalReward) public view returns(uint256){
-        uint256 totalShare = this.totalSupply + this.multiplierSupply;
-        uint256 userShare = accounts[_vault].balance + accounts[_vault].multiplier;
-        return (userShare / totalShare) * _totalReward; //TODO: might lose precision, multiply by 100 and divide back later? 
+    function getRewardsEmissions() public view returns(uint256){
+        uint256 totalStaked = this.totalSupply;
+        uint256 share = this.multiplierSupply +totalSupply;
     }
 
 
--- contracts/StakeVault.sol
+++ contracts/StakeVault.sol
@@ -1,74 +1,33 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.6.12;
+pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./StakeManager.sol";
 
+/**
+ * @title StakeVault
+ * @author Ricardo Guilherme Schmidt <ricardo3@status.im>
+ * @notice Secures user stake
+ */
 contract StakeVault {
     address owner;
     StakeManager stakeManager;
 
     ERC20 stakedToken;
-    uint256 balance;
-    uint256 locked;
-    uint256 unlockTime;
-    uint256 lockedPeriod;
-    uint256 multiplierPoints;
 
-    function join(uint256 amount) external {
-        _join(amount);
+
+    constructor(address _owner) public {
+        owner = _owner;
     }
 
-    function lock(uint256 amount, uint256 time) external {
-        _lock(amount,time);  
-    }
-
-    function joinAndLock(uint256 amount, uint256 time) external {
-        _join(amount);
-        _lock(amount,time);
-
-    }
-
-    function leave(uint256 amount) external {
-
-    }
-
-    function getMaxMultiplierPoints() public view returns(uint256) {
-        return balance * (stakeManager.MAX_BOOST() + lockup + 1);
-    }
-
-    function _join(uint256 amount) internal {
+    function join(uint256 _amount, uint256 _time) external {
         stakedToken.transferFrom(msg.sender, address(this), amount);
+        stakeManager.increaseBalance(amount, 0);
     }
 
-    function _lock(uint256 amount, uint256 time) internal {
-        require(time > 0, "Invalid lock time");
-        lockedPeriod = time;
-        unlockTime = now() + time;
-        locked = amount;
-        multiplierPoints += amount * (time + 1);
-    }
-
-    function mintMultiplierPoints() internal {
-        uint256 new_mp = multiplierPoints + (balance * stakeManager.MP_APY());
-        uint256 max_mp = getMaxMultiplierPoints();
-        multiplierPoints = new_mp > max_mp ? max_mp : new_mp;
-    }
-
-    function distriuteRewards() internal {
-            uitn256 stakeApy = stakeManager.STAKE_APY()
-        
-            if(stakeApy > 0){
-                return stake_apy * (balance + multiplierPoints)
-            } else {
-                uint256 cs = balance + multiplierPoints
-                uint256 rewards = stakeManager.getRewardEmissions();
-                if(cs > 0){
-                    return = rewards * (balance + multiplierPoints) / cs
-                }
-            }
-                
+    function leave(uint256 _amount) external {
+        stakeManager.decreaseBalance(amount);
+        stakedToken.transferFrom(address(this), msg.sender, amount);
     }
 
 }
