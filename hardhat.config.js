c487f24371db0bbf791fb8b51295851eed201e01
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2023-06-28 21:54:05 -0300

    add keys to config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
+console.log(INFURA_API_KEY)
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   mocha: {
     timeout: 100000000,
   }
+  
 };
