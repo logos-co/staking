795e76279a72880e73bc41f8f177eff49b4377eb
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2024-09-28 15:21:43 -0300

    refactor(StakeManager): Abstact out codehash verification to better code reusability
    chore(StakeManager): move out storage contract and rename it

--- certora/helpers/StakeRewardEstimateA.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-import { StakeRewardEstimate } from "./../../contracts/StakeManager.sol";
-
-contract StakeRewardEstimateA is StakeRewardEstimate {}
-
